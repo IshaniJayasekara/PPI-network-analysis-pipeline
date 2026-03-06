fa9091c2dd29eaf3d1cd7d7cb29be7e3471511ad
IshaniJayasekara <ishanijayasekara04@gmail.com>
2026-03-06 15:26:12 +0530

    Apply .gitignore and remove ignored files from tracking

--- work/5d/b3163b2223c7c4b8bfec6f08fc18cf/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-02_submodule_detection.py subnetwork.graphml modularized.graphml modules_list.xlsx 1.0
--- work/d1/c7192060953a09caf6d89d99f9e87e/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-03_hub_analysis.py modularized.graphml network_with_hubs.graphml
